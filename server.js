87fb16c011aeff61c36e6d08fd5773177479d37f
Absar Ahmad <absarahmad137@gmail.com>
2025-08-14 11:10:45 +0530

    added some styling

--- server.js
+++ server.js
@@ -1,67 +1,79 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const { MongoClient, ObjectId } = require('mongodb');
-require('dotenv').config();
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const {MongoClient, ObjectId } = require('mongodb')
+const { response } = require('express')
+const { request } = require('http')
+require('dotenv').config()
 
-const PORT = process.env.PORT || 8000;
+const PORT = 8000
 
 let db,
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'sample_mflix',
-    collection;
+    collection
 
-
-// Conected to database
 MongoClient.connect(dbConnectionStr)
     .then(client => {
-        console.log('Connected to database');
-        db = client.db(dbName);
-        collection = db.collection('movies');
+        console.log(`Connected to database`)
+        db = client.db(dbName)
+        collection = db.collection('movies')
     })
-    .catch(err => console.error(err));
 
-
-// Middleware
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended : true}))
 app.use(express.json())
 app.use(cors())
+app.use(express.static('public'));
 
 
-app.get('/search', async (request,response) => {
-    try{
 
+
+app.get("/search", async (request,response) => {
+    try {
         let result = await collection.aggregate([
             {
-                "$Search" : {
+                "$search" : {
                     "autocomplete" : {
                         "query": `${request.query.query}`,
-                        "path" : "title",
-                        "Fuzzy":{
-                            "maxEdit" : 2,
+                        "path": "title",
+                        "fuzzy": {
+                            "maxEdits":2,
                             "prefixLength": 3
                         }
                     }
                 }
             }
         ]).toArray()
-      response.send(result)
-    } catch (error){
-         response.status(500).send({message: error.message})
+        //console.log(result)
+        response.send(result)
+    } catch (error) {
+        response.status(500).send({message: error.message})
+        //console.log(error)
     }
 })
 
-app.get('/get/:id', async (request,response) => {
- try{
- let result = await collection.findOne({
-    "_id" : ObjectId(request.params.id)
- })
- response.send(result)
- }catch(error){
-  response.status(500).send({message: error.message})
- }
-})
+const { isValidObjectId } = require('mongodb');
 
-app.listen(PORT, () => {
-    console.log(`Running on port ${PORT}`);
+
+app.get("/get/:id", async (request, response) => {
+    try {
+        if (!ObjectId.isValid(request.params.id)) {
+            return response.status(400).send({ message: "Invalid ID format" });
+        }
+
+        let result = await collection.findOne({ _id: new ObjectId(request.params.id) });
+
+        if (!result) {
+            return response.status(404).send({ message: "Movie not found" });
+        }
+
+        response.send(result);
+    } catch (error) {
+        response.status(500).send({ message: error.message });
+    }
 });
+
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server is running.`)
+})
